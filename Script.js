e4740900bb8bfd0c83e9cb39909f33de52a8a073
Arun Kumar <karun74315@gmail.com>
2026-09-25 17:53:32 +0530

    changes

--- Script.js
+++ Script.js
@@ -1,9 +1,14 @@
 "use strict";
+// primitives and refrence 
 Object.defineProperty(exports, "__esModule", { value: true });
-var a = 12;
-a = "arun";
-function abs() {
-    console.log("Hello World");
-}  return a + b;
-   
+var a = 10; // primitive type
+var ab = a;
+ab++;
+console.log(a); // 10
+console.log(ab); // 11
+var b = "Arun"; // primitive type
+var c = true; // primitive type
+var d = { name: "Arun", age: 30 }; // refrence type
+var e = [1, 2, 3, 4]; // refrence type
+var ee = e;
 //# sourceMappingURL=Script.js.map
--- Script.ts
+++ Script.ts
@@ -1,7 +1,15 @@
 // primitives and refrence 
 
 var a = 10; // primitive type
+var ab = a;
+ab++;
+console.log(a); // 10
+console.log(ab); // 11
+
 var b = "Arun"; // primitive type
 var c = true; // primitive type
 
 var d = { name: "Arun", age: 30 }; // refrence type
+
+var e = [1, 2, 3, 4]; // refrence type
+var ee = e;

c54f70b8f508cef82a5d2419511a43aa4dea5ec8
Arun Kumar <karun74315@gmail.com>
2026-09-25 17:49:37 +0530

    changes

---
+++ Script.ts
@@ -0,0 +1,7 @@
+// primitives and refrence 
+
+var a = 10; // primitive type
+var b = "Arun"; // primitive type
+var c = true; // primitive type
+
+var d = { name: "Arun", age: 30 }; // refrence type
